b71be898989ece00ea7d784271b20c59682d8ff2
ezio137 <sTAtmW5b>
2016-03-21 21:04:43 -0300

    controle de versao - #10

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -2,7 +2,7 @@
 <footer class="main-footer">
     <!-- To the right -->
     <div class="pull-right hidden-xs">
-        <strong>Copyright &copy; 2016 <a href="#">Fagan</a>.</strong> Todos os direitos reservados.
+        v{{ config('app.version_number') }} [{{ config('app.version_date') }}] Copyright &copy; 2016 <strong><a href="#">Fagan</a>.</strong>
     </div>
     <!-- Default to the left -->
     @yield('botoes-footer')
