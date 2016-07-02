0f7794408497151f6340355b410ac9fd53f577d3
ezio137 <sTAtmW5b>
2016-07-02 12:30:05 -0300

    bump de versao v0.7.0

--- config/app.php
+++ config/app.php
@@ -3,8 +3,8 @@ setlocale(LC_TIME, 'pt_BR.UTF-8');
 
 return [
 
-    'version_number' => '0.6.3',
-    'version_date' => '01/07/2016',
+    'version_number' => '0.7.0',
+    'version_date' => '02/07/2016',
 
     'env' => env('APP_ENV', 'production'),
 
