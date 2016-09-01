70649c0cc64816b91fafe994d8d7fa61625a7273
ezio137 <sTAtmW5b>
2016-08-31 23:33:33 -0300

    Upgrade Laravel 5.3 #39

--- config/app.php
+++ config/app.php
@@ -6,6 +6,29 @@ return [
     'version_number' => '0.9.0',
     'version_date' => '06/07/2016',
 
+    /*
+    |--------------------------------------------------------------------------
+    | Application Name
+    |--------------------------------------------------------------------------
+    |
+    | This value is the name of your application. This value is used when the
+    | framework needs to place the application's name in a notification or
+    | any other location as required by the application or its packages.
+    */
+
+    'name' => 'My Application',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Environment
+    |--------------------------------------------------------------------------
+    |
+    | This value determines the "environment" your application is currently
+    | running in. This may determine how you prefer to configure various
+    | services your application utilizes. Set this in your ".env" file.
+    |
+    */
+
     'env' => env('APP_ENV', 'production'),
 
     /*
@@ -32,7 +55,7 @@ return [
     |
     */
 
-    'url' => 'http://localhost',
+    'url' => env('APP_URL', 'http://localhost'),
 
     /*
     |--------------------------------------------------------------------------
@@ -84,7 +107,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', 'SomeRandomString'),
+    'key' => env('APP_KEY'),
 
     'cipher' => 'AES-256-CBC',
 
@@ -101,7 +124,9 @@ return [
     |
     */
 
-    'log' => 'single',
+    'log' => env('APP_LOG', 'single'),
+
+    'log_level' => env('APP_LOG_LEVEL', 'debug'),
 
     /*
     |--------------------------------------------------------------------------
@@ -131,6 +156,7 @@ return [
         Illuminate\Foundation\Providers\FoundationServiceProvider::class,
         Illuminate\Hashing\HashServiceProvider::class,
         Illuminate\Mail\MailServiceProvider::class,
+        Illuminate\Notifications\NotificationServiceProvider::class,
         Illuminate\Pagination\PaginationServiceProvider::class,
         Illuminate\Pipeline\PipelineServiceProvider::class,
         Illuminate\Queue\QueueServiceProvider::class,
@@ -142,10 +168,17 @@ return [
         Illuminate\View\ViewServiceProvider::class,
 
         /*
+         * Package Service Providers...
+         */
+
+        //
+
+        /*
          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
+        // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
@@ -173,7 +206,6 @@ return [
         'Artisan' => Illuminate\Support\Facades\Artisan::class,
         'Auth' => Illuminate\Support\Facades\Auth::class,
         'Blade' => Illuminate\Support\Facades\Blade::class,
-        'Bus' => Illuminate\Support\Facades\Bus::class,
         'Cache' => Illuminate\Support\Facades\Cache::class,
         'Config' => Illuminate\Support\Facades\Config::class,
         'Cookie' => Illuminate\Support\Facades\Cookie::class,
@@ -184,11 +216,10 @@ return [
         'File' => Illuminate\Support\Facades\File::class,
         'Gate' => Illuminate\Support\Facades\Gate::class,
         'Hash' => Illuminate\Support\Facades\Hash::class,
-        'Input' => Illuminate\Support\Facades\Input::class,
-        'Inspiring' => Illuminate\Foundation\Inspiring::class,
         'Lang' => Illuminate\Support\Facades\Lang::class,
         'Log' => Illuminate\Support\Facades\Log::class,
         'Mail' => Illuminate\Support\Facades\Mail::class,
+        'Notification' => Illuminate\Support\Facades\Notification::class,
         'Password' => Illuminate\Support\Facades\Password::class,
         'Queue' => Illuminate\Support\Facades\Queue::class,
         'Redirect' => Illuminate\Support\Facades\Redirect::class,
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
-var elixir = require('laravel-elixir');
+const elixir = require('laravel-elixir');
+
+require('laravel-elixir-vue');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,35 +13,36 @@ var elixir = require('laravel-elixir');
  |
  */
 
-elixir(function (mix) {
-    mix.sass('app.scss');
+elixir(mix => {
+    mix.sass('app.scss')
+       .webpack('app.js');
 
-    
-    // jQuery
-    mix.copy('node_modules/jquery/dist/jquery.js', 'public/vendor/jquery');
 
-    // Bootstrap
-    mix.copy('node_modules/bootstrap/dist/css/bootstrap.css', 'public/vendor/bootstrap');
-    mix.copy('node_modules/bootstrap/dist/js/bootstrap.js', 'public/vendor/bootstrap');
+// jQuery
+mix.copy('node_modules/jquery/dist/jquery.js', 'public/vendor/jquery');
 
-    // Select2
-    mix.copy('node_modules/select2/dist/css/select2.css', 'public/vendor/select2');
-    mix.copy('node_modules/select2/dist/js/select2.js', 'public/vendor/select2');
+// Bootstrap
+mix.copy('node_modules/bootstrap/dist/css/bootstrap.css', 'public/vendor/bootstrap');
+mix.copy('node_modules/bootstrap/dist/js/bootstrap.js', 'public/vendor/bootstrap');
 
-    // Bootstrap Datepicker
-    mix.copy('node_modules/bootstrap-datepicker/dist/css/bootstrap-datepicker.css', 'public/vendor/bootstrap-datepicker');
-    mix.copy('node_modules/bootstrap-datepicker/dist/js/bootstrap-datepicker.js', 'public/vendor/bootstrap-datepicker');
+// Select2
+mix.copy('node_modules/select2/dist/css/select2.css', 'public/vendor/select2');
+mix.copy('node_modules/select2/dist/js/select2.js', 'public/vendor/select2');
 
-    // AdminLTE
-    mix.copy('node_modules/admin-lte/dist/css/AdminLTE.css', 'public/vendor/admin-lte');
-    mix.copy('node_modules/admin-lte/dist/js/app.js', 'public/vendor/admin-lte');
-    mix.copy('node_modules/admin-lte/dist/css/skins/*.css', 'public/vendor/admin-lte/skins');
+// Bootstrap Datepicker
+mix.copy('node_modules/bootstrap-datepicker/dist/css/bootstrap-datepicker.css', 'public/vendor/bootstrap-datepicker');
+mix.copy('node_modules/bootstrap-datepicker/dist/js/bootstrap-datepicker.js', 'public/vendor/bootstrap-datepicker');
 
-    // Vue
-    mix.copy('node_modules/vue/dist/vue.js', 'public/vendor/vue');
+// AdminLTE
+mix.copy('node_modules/admin-lte/dist/css/AdminLTE.css', 'public/vendor/admin-lte');
+mix.copy('node_modules/admin-lte/dist/js/app.js', 'public/vendor/admin-lte');
+mix.copy('node_modules/admin-lte/dist/css/skins/*.css', 'public/vendor/admin-lte/skins');
 
-    mix.copy('resources/assets/css/styles.css', 'public/css');
-    mix.copy('resources/assets/css/style_v1.css', 'public/css');
-    mix.copy('resources/assets/js/devoops.js', 'public/js');
-    mix.copy('resources/assets/img', 'public/img');
+// Vue
+mix.copy('node_modules/vue/dist/vue.js', 'public/vendor/vue');
+
+mix.copy('resources/assets/css/styles.css', 'public/css');
+mix.copy('resources/assets/css/style_v1.css', 'public/css');
+mix.copy('resources/assets/js/devoops.js', 'public/js');
+mix.copy('resources/assets/img', 'public/img');
 });
